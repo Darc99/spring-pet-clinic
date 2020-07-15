df8905ba9cf6e195aff0750e038ee7afa9e09d19
Darc99 <lennyyhugoh@gmail.com>
2020-07-15 23:23:25 +0100

    upgraded to junit 5. close #4

--- pet-clinic-web/src/test/java/first/springframework/springpetclinic/SpringPetClinicApplicationTests.java
+++ pet-clinic-web/src/test/java/first/springframework/springpetclinic/SpringPetClinicApplicationTests.java
@@ -2,8 +2,11 @@ package first.springframework.springpetclinic;
 
 import first.springframework.springpetclinic.SpringPetClinicApplication;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+@ExtendWith(SpringExtension.class)
 @SpringBootTest(classes = SpringPetClinicApplication.class)
 class SpringPetClinicApplicationTests {
 
