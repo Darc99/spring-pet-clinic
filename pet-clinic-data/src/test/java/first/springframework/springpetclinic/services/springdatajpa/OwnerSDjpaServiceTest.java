d2817ad82f710992c51c50f792e4a5741e860a7c
Darc99 <lennyyhugoh@gmail.com>
2020-07-21 22:11:44 +0100

    created owner sd jpa service test

---
+++ pet-clinic-data/src/test/java/first/springframework/springpetclinic/services/springdatajpa/OwnerSDjpaServiceTest.java
@@ -0,0 +1,46 @@
+package first.springframework.springpetclinic.services.springdatajpa;
+
+import first.springframework.springpetclinic.repositories.OwnerRepository;
+import first.springframework.springpetclinic.repositories.PetRepository;
+import first.springframework.springpetclinic.repositories.PetTypeRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OwnerSDjpaServiceTest {
+
+    OwnerRepository ownerRepository;
+    PetRepository petRepository;
+    PetTypeRepository petTypeRepository;
+
+    OwnerSDjpaService service;
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void findByLastName() {
+    }
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void save() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+    @Test
+    void deleteById() {
+    }
+}
